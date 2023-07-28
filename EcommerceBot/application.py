060bfbb2f8e74ace9319ec4e59303f61b2e8985d
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2023-07-28 09:49:32 +0900

    Add files via upload

--- EcommerceBot/application.py
+++ EcommerceBot/application.py
@@ -1,48 +1,36 @@
 from flask import Flask, render_template, request, jsonify
 import boto3
-from collections import deque
 from datetime import datetime
 application = Flask(__name__)
-dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2', aws_access_key_id='AKIARWKDYXU5AF5MOEIP', aws_secret_access_key='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')
+# dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2', aws_access_key_id='', aws_secret_access_key='')
+dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2', aws_access_key_id='', aws_secret_access_key='')
 table = dynamodb.Table('buy_product')
 
 
 @application.route("/")
 def product():
-    return render_template("product.html")
-
+    return render_template("cart.html")
 
 @application.route("/buy", methods=['POST', 'GET'])
 def buy():
-    if request.method == 'POST':
-        buy_product = request.form
-        buy_list = []
+    if request.method=='POST':
+        buy_product=request.form
+        buy_data={}
         for key, value in buy_product.items():
-            if value == 'on':
-                buy_list.append(key)
-            elif key.find('수량') != -1:
-                if value == '':
-                    continue
-                buy_list.append(int(value))
-        buy_list = deque(buy_list)
-
-        buy_data = {}
-        while len(buy_list) != 0:
-            buy_data['name'] = buy_list.popleft()
-            buy_data['count'] = buy_list.popleft()
-            buy_today = datetime.today()
+            data=value.split('\t')
+            buy_today=datetime.today()
             buy_date = str(buy_today.year)+'-'+str(buy_today.month)+'-'+str(buy_today.day)
-            buy_data['date'] = buy_date
+            buy_data['name'], buy_data['count'], buy_data['date']=data[0],data[-1], buy_date
             table.put_item(Item=buy_data)
-            buy_data = {}
-        return render_template("buy.html", result=buy_product)
+            buy_data={}
+        return render_template("buy.html")
 
 
 @application.route("/chatbot", methods=["POST", "GET"])
 def chatbot():
     if request.method == 'POST':
         input_message = request.json.get('inputValue')
-        client = boto3.client('lexv2-runtime', region_name='ap-northeast-2', aws_access_key_id='AKIARWKDYXU5AF5MOEIP', aws_secret_access_key='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')
+        client = boto3.client('lexv2-runtime', region_name='ap-northeast-2', aws_access_key_id='', aws_secret_access_key='')
         botId = "GI0U5JJUYN"
         botAliasId = "TSTALIASID"
         localeId = "ko_KR"
@@ -67,11 +55,6 @@ def chatbot():
             return jsonify({"message_response1": message_response1})
     else:
         return render_template('chatbot.html')
-
-
-@application.route("/buy_product")
-def buy_product():
-    return render_template('buy_product.html')
 
 
 if __name__ == "__main__":
